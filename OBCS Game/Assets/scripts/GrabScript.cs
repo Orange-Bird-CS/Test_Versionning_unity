c7e905f4caa52db682fb2b4c2a956a6a31b7b543
ChristCay <33967929+ChristCay@users.noreply.github.com>
2018-05-22 18:57:11 +0200

    Robots Animation

---
+++ OBCS Game/Assets/scripts/GrabScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabScript : MonoBehaviour
+{
+
+    public bool grabbed;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public Transform holdpoint;
+    public Transform ground;
+    public float throwforce;
+    public LayerMask notgrabbed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!grabbed)
+            {
+                //Grab
+                Physics2D.queriesStartInColliders = false;
+                hit = Physics2D.Raycast(ground.position, Vector2.right * transform.localScale.x, distance);
+
+                if (hit.collider != null && hit.collider.tag == "Grabbable")
+                {
+                    grabbed = true;
+                }
+            }
+            else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
+            {
+                //Throw
+                grabbed = false;
+                if (hit.collider.gameObject.GetComponent<BoxCollider2D>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                }
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                }
+            }
+        }
+
+        if (grabbed)
+        {
+            hit.collider.gameObject.transform.position = holdpoint.position;
+            if (hit.collider.gameObject.GetComponent<BoxCollider2D>() != null)
+            {
+                hit.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+    }
+}
---
+++ OBCS Game/Assets/scripts/Parallax.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+    public Transform[] Decors;
+    private float[] parallaxScales;
+    public float smoothing;
+    public float smooth_y = 0.5f;
+
+    private Vector3 previousCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+
+        previousCameraPosition = transform.position;
+
+        parallaxScales = new float[Decors.Length];
+        for(int i = 0; i < parallaxScales.Length; i++)
+        {
+
+            parallaxScales[i] = Decors[i].position.z * -1;
+
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+        for(int i = 0; i < Decors.Length; i++)
+        {
+            Vector3 parallax = (previousCameraPosition - transform.position) * (parallaxScales[i] / smoothing);
+
+            Decors[i].position = new Vector3(Decors[i].position.x + parallax.x, Decors[i].position.y + parallax.y * smooth_y, Decors[i].position.z);
+
+        }
+
+        previousCameraPosition = transform.position;
+
+	}
+}
